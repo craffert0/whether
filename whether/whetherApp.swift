664ea8aa69b276b109e44d9ce98308f032157635
Colin Rafferty <colin@rafferty.net>
2022-11-21 11:23:15 -0500

    initial template

---
+++ whether/whetherApp.swift
@@ -0,0 +1,17 @@
+//
+//  whetherApp.swift
+//  whether
+//
+//  Created by Colin Rafferty on 11/20/22.
+//
+
+import SwiftUI
+
+@main
+struct whetherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
