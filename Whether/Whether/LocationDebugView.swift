f6cf2e10752c2226fc96affcfec2226b588b7229
Colin Rafferty <colin@rafferty.net>
2025-04-27 22:17:58 -0400

    map LocationDebugView

--- Whether/Whether/LocationDebugView.swift
+++ Whether/Whether/LocationDebugView.swift
@@ -8,12 +8,30 @@ struct LocationDebugView: View {
     @Environment(WhetherModel.self) private var model
 
     var body: some View {
+        map
+        list
+    }
+
+    var map: some View {
+        Map {
+            MapPolyline(
+                coordinates: model.locations.elements.map(\.item.coordinate)
+            )
+
+            ForEach(model.locations.elements) { e in
+                Marker(e.time.plain_time, coordinate: e.item.coordinate)
+            }
+        }
+    }
+
+    var list: some View {
         ScrollView {
             Grid {
                 ForEach(model.locations.elements.reversed()) { e in
+                    let c = e.item.coordinate
                     GridRow {
                         Text(e.time.plain_time)
-                        Button("\(e.item.coordinate.latitude)/\(e.item.coordinate.longitude)") {
+                        Button("\(c.latitude)/\(c.longitude)") {
                             let placemark =
                                 MKPlacemark(coordinate: e.item.coordinate)
                             let mapItem = MKMapItem(placemark: placemark)
@@ -24,5 +42,12 @@ struct LocationDebugView: View {
                 }
             }
         }
+    }
+}
+
+#Preview {
+    VStack {
+        LocationDebugView()
+            .environment(WhetherModel())
     }
 }
